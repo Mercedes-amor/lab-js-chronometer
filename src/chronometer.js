cea57562b172c854befdb9ae1395cc37dfda255a
MercedesAmorGallart <127596167+MercedesAmorGallart@users.noreply.github.com>
2023-07-18 17:56:11 +0200

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,57 @@
+
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId =setInterval (() => {
+      this.currentTime++;
+      if (typeof printTimeCallback === "function"){
+      printTimeCallback()
+    }
+    },1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes =Math.floor(this.currentTime/60)
+    return minutes
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds=0;
+    
+    if (this.currentTime < 60) {
+    return this.currentTime
+    }
+    if (this.currentTime >= 60) {
+    seconds = this.currentTime % 60
+    return seconds  
+    }
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let twoDigit;
+    twoDigit = value.toString()
+    if (twoDigit.length < 2) {
+    return "0"+ twoDigit
+    }
+    else {
+    return twoDigit
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+   
+   return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`  
   }
 }
